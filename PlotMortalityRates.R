c2d9748dca261891c155674bfc88c8a26c5ac48e
[Arly Berg] <[Arly@ituniq.dk]>
2021-03-26 10:03:51 +0100

    plot function implemented

--- PlotMortalityRates.R
+++ PlotMortalityRates.R
@@ -1,9 +1,32 @@
+## ----------------------------------------------------------------------------
+## Coursera - R Programming
+## Programming assignment 3
+## 21-03-2021 Arly Berg
+## ----------------------------------------------------------------------------
+
+## ----------------------------------------------------------------------------
+## This file contains functions for plotting the rates for spicific state
+## -- Load data (filename)
+## -- get.data (x, index)
+## ----------------------------------------------------------------------------
+
+
 source("HospitalUtilities.R")
 
-hospital.data.raw <- hospital.load("outcome-of-care-measures.csv")
-hospital.data.cleaned <- get.data(hospital.data.raw, dataset.heart.attack)
+hospital.state <- function(state, rate = dataset.heart.attack, ordered = F){
+  
+  # load data
+  hospital.data.raw <- hospital.load("outcome-of-care-measures.csv")
+  hospital.data.cleaned <- get.data(hospital.data.raw, rate)
+  
+  rates.state <- hospital.data.cleaned[hospital.data.cleaned$State == state,]
+  
+  if (ordered == T){
+    rates.state <- rates.state[order(rates.state[,3]),]
+  } 
 
-rates.state <- hospital.data.cleaned[hospital.data.cleaned$State == "AZ",]
+  return(rates.state)  
+}
 
-rates.ordered <- order(rates.state[,3])
-plot(rates.ordered)
+
+plot(hospital.state("TX", rate = dataset.heart.failures, ordered = T)[,3])
