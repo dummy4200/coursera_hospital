07d8b9342091400d22352d2a461597500d3983da
[Arly Berg] <[Arly@ituniq.dk]>
2021-03-26 21:18:27 +0100

    best.R implmented

--- best.R
+++ best.R
@@ -24,12 +24,23 @@ outcomes <- c("heart attack", "heart failure", "pneumonia")
 ## output: a character vector - name of the hospital with the lowest mortality rate
 ## ----------------------------------------------------------------------------
 best <- function(state, outcome) {
+  ## Check that state and outcome are valid
+  if (!state %in% state.abb){
+    stop("Not a valid state")
+  }
+  
+  if (!outcome %in% outcomes){
+    stop("Not a valid outcome")
+  }
+  
   ## Read outcome data
   hospital.raw <- hospital.load("outcome-of-care-measures.csv")  
-  
-  ## Check that state and outcome are valid
+  index <- col.index[match(outcome,outcomes)]
+  hospital.values <- get.data(hospital.raw, index)
+  hospital.selected <- hospital.values[hospital.values$State == state,]
   
   ## Return hospital name in that state with lowest 30-day death
-  
+  return (hospital.selected[which.min(hospital.selected[,3]),]$Hospital.Name)
   ## rate
 }
+
